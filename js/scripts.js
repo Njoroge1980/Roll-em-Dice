52754d930c111b580e47d70dc911f7926e6721bd
Njoroge Mungai <njorogemungai1980@gmail.com>
2017-08-25 17:36:43 +0300

    add missing styling on the deployed site

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,5 @@ this.total= total;
 
 var player1 = new Player ("Mike", 0);
 var player2 = new Player ("Joe", 0);
-var die = parseInt (Math.random() *6) + 1; {
-
-}
-
-
-var totalScore=
+var die = parseInt (Math.random() *6) + 1;
+var totalScore = function() totalScore += die
