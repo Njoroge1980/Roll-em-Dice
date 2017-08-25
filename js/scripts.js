5ceb30965cb646dfa2a4786dea1bfdeb9a39edaf
Njoroge Mungai <njorogemungai1980@gmail.com>
2017-08-25 17:10:18 +0300

    add scripts to the webpage add functionality for the page

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+function Player (name,total) {
+this.name =name;
+this.total= total;
+}
+
+var player1 = new Player ("Mike", 0);
+var player2 = new Player ("Joe", 0);
+var die = parseInt (Math.random() *6) + 1; {
+  return dice
+}
+
+
+var totalScore= 
