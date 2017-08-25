f7c1b4d2dc402377ca013041862d2f47b970eda0
Njoroge Mungai <njorogemungai1980@gmail.com>
2017-08-25 17:25:18 +0300

    add variables in js

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ this.total= total;
 var player1 = new Player ("Mike", 0);
 var player2 = new Player ("Joe", 0);
 var die = parseInt (Math.random() *6) + 1; {
-  return dice
+
 }
 
 
-var totalScore= 
+var totalScore=
